cfda4d4b8b9fee2d616b8667c362ff34b7d85b36
angelacpd <angelacrepaldi@gmail.com>
2021-08-09 19:11:21 -0300

    Uninstall python-dotenv. Install django-environ. Add CORS_ORIGIN_WHITELIST to .env.

--- backend/todo_project/settings.py
+++ backend/todo_project/settings.py
@@ -12,22 +12,30 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-# Environment variables
-from dotenv import load_dotenv
-load_dotenv()
+import environ
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Environment variables
+env = environ.Env(
+    DEBUG=(bool, False),
+    SECRET_KEY=(str, ''),
+    CORS_ORIGIN_WHITELIST=(tuple, ())
+)
+
+dotenv_file_path = os.path.join(BASE_DIR, '.env')
+environ.Env.read_env(dotenv_file_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.getenv('DEBUG'))
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -143,7 +151,4 @@ REST_FRAMEWORK = {
     ]
 }
 
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
-    'http://localhost:8000',
-)
+CORS_ORIGIN_WHITELIST = env.tuple('CORS_ORIGIN_WHITELIST')
