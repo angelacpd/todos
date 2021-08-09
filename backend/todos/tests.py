511268bf71e37eef4aa23ee73cb51aeef42b6b27
angelacpd <angelacrepaldi@gmail.com>
2021-08-08 22:28:40 -0300

    Add tests.

--- backend/todos/tests.py
+++ backend/todos/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+from todos.models import Todo
+
+
+class TodoModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.title = 'first todo'
+        cls.body = 'a body here'
+        Todo.objects.create(title=cls.title, body=cls.body)
+
+    def test_title_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.title}'
+        self.assertEqual(expected_object_name, self.title)
+        expected_object_body = f'{todo.body}'
+        self.assertEqual(expected_object_body, self.body)
